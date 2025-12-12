3df8b58b58090b8840ab1646290975a3c62b45e8
Tenkeyyy <mezyramzi@gmail.com>
2025-12-12 23:06:33 +0100

    Adding APIs endpoints + implementing JWT Authentication

--- Algogrow/Algogrow/urls.py
+++ Algogrow/Algogrow/urls.py
@@ -4,4 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("" , include("api.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair_view'),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('get_stats', views.get_stats),
 ]
