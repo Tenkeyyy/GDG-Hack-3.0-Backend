a5a614e714eb79aadf4f2fa394e52d043e2bf30e
Tenkeyyy <mezyramzi@gmail.com>
2025-12-13 11:55:10 +0100

    Last commit

--- Algogrow/api/views.py
+++ Algogrow/api/views.py
@@ -20,7 +20,7 @@ def growth(x,y):
 
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, username = request.data['username'])
+    user = get_object_or_404(User, email = request.data['email'])
     if not user.check_password(request.data['password']):
         return Response({"detail" : "Not found."}, status=status.HTTP_404_NOT_FOUND)
     token , created = Token.objects.get_or_create(user=user)
